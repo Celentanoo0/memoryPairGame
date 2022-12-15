dc81c7780e5cbbbb74f174a77077397a1ad81957
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2022-12-15 20:47:35 +0200

    [features-small-improvements] reload page after winning

--- js/script.js
+++ js/script.js
@@ -4,44 +4,6 @@ const cards = Array.from(document.querySelectorAll(".flipper"));
 const cardsImg = Array.from(document.querySelectorAll(".card__front img"));
 const selectedCards = [];
 
-for (const elem of cards) {
-    elem.addEventListener("click", slider);
-}
-
-function slider(e){
-    if(selectedCards.length < 2){
-        e.target.closest('.flipper').classList.toggle("flip");
-        selectedCards.push(e.target);
-        e.target.closest('.flipper').removeEventListener('click', slider);
-    }
-    if(selectedCards.length >= 2){
-        checkout();
-    }
-}
-
-function checkout(){
-    if(selectedCards[0].querySelector('img').src === selectedCards[1].querySelector('img').src){
-        selectedCards.forEach(item => item.classList.toggle('solved'))
-        selectedCards.length = 0;
-    }
-    else setTimeout(() => {
-        selectedCards.forEach((item) => {
-            item.closest('.flipper').classList.toggle("flip");
-            item.closest('.flipper').addEventListener("click", slider);
-        })
-        selectedCards.length = 0;
-    }, 1000);
-}
-
-const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-};
-
 const images = [
     "img/1.png",
     "img/2.png",
@@ -57,6 +19,69 @@ const images = [
     "img/6.png",
 ];
 
+for (const elem of cards) {
+    elem.addEventListener("click", slider);
+}
+
+function slider(e) {
+    if (selectedCards.length < 2) {
+        e.target.closest(".flipper").classList.toggle("flip");
+        selectedCards.push(e.target);
+        e.target.closest(".flipper").removeEventListener("click", slider);
+    }
+    if (selectedCards.length >= 2) {
+        checkout();
+    }
+}
+
+function checkout() {
+    if (
+        selectedCards[0].querySelector("img").src ===
+        selectedCards[1].querySelector("img").src
+    ) {
+        selectedCards.forEach((item) => item.classList.toggle("solved"));
+        images.length -= 2;
+        selectedCards.length = 0;
+        if (images.length <= 0) {
+            winnerFunc();
+        }
+    } else
+        setTimeout(() => {
+            selectedCards.forEach((item) => {
+                item.closest(".flipper").classList.toggle("flip");
+                item.closest(".flipper").addEventListener("click", slider);
+            });
+            selectedCards.length = 0;
+        }, 1000);
+}
+
+function winnerFunc() {
+    const winner = document.createElement("div");
+    winner.classList.add("winner");
+    const winnerText = document.createElement("p");
+    winnerText.innerHTML = "Congratulations, You Win!";
+    const tryAgainBtn = document.createElement("div");
+    tryAgainBtn.classList.add("winner__btn");
+    tryAgainBtn.innerHTML = "Try again";
+    tryAgainBtn.addEventListener("click", function refresh() {
+        document.location.reload(true);
+    });
+    winner.append(winnerText);
+    winner.append(tryAgainBtn);
+    setTimeout(() => {
+        document.querySelector(".header").remove();
+        document.querySelector(".wrapper").append(winner);
+    }, 1000);
+}
+
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+};
 shuffleArray(images);
 
 let i = 0;
